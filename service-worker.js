e35c0169db00c8d67499b9ffac7c96bf27d12c80
rajasekhar056 <krajasekhar056@gmail.com>
2023-11-23 10:31:35 +0530

    fixed service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,12 @@
 // service-worker.js
+/* eslint-disable no-undef, no-restricted-globals */
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
 const CACHE_NAME = 'my-app-cache-v1';
 const urlsToCache = ['/index.html', '/static/css/main.css', '/static/js/main.js'];
 
-this.addEventListener('install', (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
@@ -10,7 +14,7 @@ this.addEventListener('install', (event) => {
   );
 });
 
-this.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
